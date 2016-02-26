f14637028423bdd6e750ebe6cb97104eb17545cc
Josué Abenamar <josueabenamar.dev@gmail.com>
2016-02-26 16:04:07 -0600

    structure changes

--- auth/config.py
+++ auth/config.py
@@ -1,7 +1,7 @@
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'data/auth.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'auth.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 DATABASE_CONNECT_OPTIONS = {}
 
