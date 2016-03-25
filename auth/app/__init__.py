98e07186c2f4e6f51945842f18578af16fb404bd
Jorge Raze <jorge.raze@clevertech.biz>
2016-03-25 15:25:34 -0600

    Try login manager

--- auth/app/__init__.py
+++ auth/app/__init__.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+import flask.ext.login as flask_login
+
 from app.mod_base.access import set_cors
 from app.mod_base.access import set_errors
 
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 set_cors(app)
 set_errors(app)
 
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
 db = SQLAlchemy(app)
 
 from app.mod_auth.controllers import auth_module
