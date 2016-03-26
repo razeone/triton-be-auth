0e8aade19d3b03c7c7d69759a93fb398dcbe5154
Jorge Raze <jorge.raze@clevertech.biz>
2016-03-25 18:18:49 -0600

    Add postgresql for production environment

--- auth/config.py
+++ auth/config.py
@@ -5,11 +5,22 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 HOST = "0.0.0.0"
 PORT = 8085
-DEBUG = True
+DEBUG = False
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'auth.db')
+POSTGRES_USER = os.environ.get('POSTGRES_USER', 'postgres')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'secret')
+POSTGRES_HOST = 'localhost'
+POSTGRES_DATABASE_NAME = os.environ.get('POSTGRES_DATABASE_NAME', 'triton_auth')
+
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://' + POSTGRES_USER + ':' + POSTGRES_PASSWORD + '@' + POSTGRES_HOST + '/' + POSTGRES_DATABASE_NAME
+
+if DEBUG:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'auth.db')
+
+
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-DATABASE_CONNECT_OPTIONS = {}
+
 
 SECRET_KEY = "tritondevs"
 ENCRYPTION_ALGORITHM = 'HS256'
