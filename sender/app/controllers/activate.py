b0bf1c43313f2bf9524099d4477351f5ce4d622a
Josué Abenamar <josueabenamar.dev@gmail.com>
2016-05-03 19:52:47 -0500

    mail with templates

--- sender/app/controllers/activate.py
+++ sender/app/controllers/activate.py
@@ -5,6 +5,8 @@ import config
 
 channel = "activate"
 
+mail_template = "4fc98bc0-7187-49f4-9bd1-7b18d105577b"
+
 subject = "Activate user account"
 user_from = "activate@triton.dev"
 activate_url = "http://" + config.HOSTNAME + "/v1/auth/activate/"
@@ -23,8 +25,13 @@ def handle_message(content):
         mail.add_to(user_to)
         mail.set_from(user_from)
         mail.set_subject(subject)
+        mail.add_filter("templates", "enable", "1")
+        mail.add_filter("templates", "template_id", mail_template)
+        mail.add_substitution(":url", activate_url + user_token)
         mail.set_html(activate_url + user_token)
-        mailClient.send(mail)
+        status = mailClient.send(mail)
+
+        print(status)
 
     except Exception as e:
         print(e)
--- sender/app/controllers/recover.py
+++ sender/app/controllers/recover.py
@@ -5,6 +5,8 @@ import config
 
 channel = "recover"
 
+mail_template = "7016339e-6067-4464-86a1-5180c3eb3724"
+
 subject = "Recover user password"
 user_from = "recover@triton.dev"
 recover_url = "http://" + config.HOSTNAME + "/user/#/reset/"
@@ -23,8 +25,13 @@ def handle_message(content):
         mail.add_to(user_to)
         mail.set_from(user_from)
         mail.set_subject(subject)
+        mail.add_filter("templates", "enable", "1")
+        mail.add_filter("templates", "template_id", mail_template)
+        mail.add_substitution(":url", recover_url + user_token)
         mail.set_html(recover_url + user_token)
-        mailClient.send(mail)
+        status = mailClient.send(mail)
+
+        print(status)
 
     except Exception as e:
         print(e)
